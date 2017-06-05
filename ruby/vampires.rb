de586fb14dfef96c2d96bf4208465a334177ed82
Ralph Tyrell <rtyrellcode@gmail.com>
2017-06-04 19:49:40 -0700

    Complete 4.4 Solo challange.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,18 +1,29 @@
 def vamp_detection
-#set variables for logic tests"
+puts "How many candidates will you be interviewing? (enter a number)"
+  number = gets.chomp
 
+  index = 0
+  candidates = number.to_i
+
+while index < candidates
+#set variables for logic tests"
   print "What is your name?"
   name = gets.chomp
-  case name
-  when "Drake Cula", "Tu Fang"
-    puts "You may be a vampire!"
-  end
+#  case name
+#  when "Drake Cula", "Tu Fang"
+#    puts "You may be a vampire!"
+#  end
   print "How old are you?"
   age = gets.chomp
   age = age.to_i
   print "What year were you born?"
   yob = gets.chomp
   yob = yob.to_i
+  if Time.new.year - yob == age
+    age_correct = true
+    else
+    age_correct = false
+  end
 
   print "Our company cafeteria serves garlic bread. Should we order some garlic bread for you? (y/n)"
   garlic = gets.chomp
@@ -31,14 +42,39 @@ def vamp_detection
          insurance = false
        else
          puts "Please answer 'y' or 'n'"
-       end
 
-  if Time.new.year - yob == age &&
+       end
+       index += 1
+  end
+
+  alergies = "hay"
+
+  while alergies != "sunshine" || "done"
+  puts "Please list any alergies! Enter 'done' when complete."
+  alergies = gets.chomp
+    if alergies == "sunshine"
+      puts "Probably a vampire."
+      break
+  elsif alergies == "done"
+    puts "Thank you for this information."
+    break
+  else
+    puts "Please enter alergy."
   end
 end
-# insurance = gets.chomp
-# puts "name: #{name}"
-# puts "age : #{age}"
-# puts "yob: #{yob}"
-# puts "garlic: #{garlic}"
-# puts "insurance: #{insurance}"
+
+
+  if age_correct == true && (garlic == true || insurance == true)
+    puts "Probably not a vampire"
+  elsif age_correct && (insurance = true || garlic = true)
+    puts "Probably not a vampire."
+  elsif age_correct == false && garlic == false && insurance == false
+    puts "Amost certainly a vampire."
+  elsif name == "Drake Cula" || "Tu Fang"
+    puts "Definitely a vampire"
+  else
+    puts "Results inconclusive"
+
+  end
+
+end
