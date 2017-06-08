989a3b44f6e435fa762cfa8c2b773fa320570b4e
Ralph Tyrell <rtyrellcode@gmail.com>
2017-06-07 19:35:53 -0700

    Add initial code block for hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,25 @@
+
+Pseudocode
+Description:
+  hash structure based designer "client_profile"
+  inputs: contact data, family info, taste and preferences
+  outputs: user opportunity to correct mistakes, client interview report
+Establish hash [client_profile]
+  Use symbols for default values
+  driver code for corrections and updates
+  print report
+
+
+  client_profile = {
+    name: "Meredit Vanderbilt",
+    phone: "(858)621-7721",
+    email: "mvanderb@cox.com",
+    age: 37,
+    children: 3,
+    decor_theme: "Novo Andalucian",
+    proposal_sent: TRUE,
+    referral: "Maxwell Bradley",
+
+  }
+
+client_profile
